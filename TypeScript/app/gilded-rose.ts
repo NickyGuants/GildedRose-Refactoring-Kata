30dc41b29b4c6fabd218fec082bf4da14f976b38
NickyGuants <nicholasguantai528@gmail.com>
2022-09-26 15:33:33 +0300

    refactor kata

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -3,7 +3,7 @@ export class Item {
   sellIn: number;
   quality: number;
 
-  constructor(name: string, sellIn: number, quality: number) {
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -17,79 +17,53 @@ export class GildedRose {
     this.items = items;
   }
 
-  // Normal Items
-  updateCustomItemQuality(item: Item) {
-    if (item.quality > 0) {
-      if (item.sellIn < 1 && item.quality > 1) {
-         item.quality -= 2;
-      } else {
-        item.quality -= 1;
-       }
-    }
-    item.sellIn = item.sellIn - 1;
-    return item;
-  }
-
-  //Aged brie items
-  updateAgedBrieQuality(item: Item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
-    item.sellIn = item.sellIn - 1;
-    return item;
-  }
-
-  //Backstage Items
-  updateBackstageQuality(item: Item) {
-    if (item.quality < 50) {
-      if (item.sellIn < 0) {
-        item.quality = item.quality - item.quality;
-      } else if (item.sellIn < 6) {
-        item.quality = item.quality + 3;
-      } else if (item.sellIn < 11) {
-        item.quality = item.quality + 2;
-      } else {
-        item.quality = item.quality + 1;
-      }
-      item.sellIn = item.sellIn - 1;
-    }
-    return item;
-  }
-
-  //Sulfuras (Legendary items)
-  updateLegendaryItemQuality(item: Item) {
-    return item;
-  }
-
-  //New feature - Conjured Items
-  updateConjuredItemQuality(item: Item) {
-    if (item.quality > 0) {
-       item.quality = item.quality - 2;
-    }
-    item.sellIn = item.sellIn - 1;
-    return item;
-  }
-
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      let name = this.items[i].name;
-      switch (name) {
-        case 'Aged Brie':
-          this.items[i] = this.updateAgedBrieQuality(this.items[i]);
-          break;
-        case 'Backstage passes to a TAFKAL80ETC concert':
-          this.items[i] = this.updateBackstageQuality(this.items[i]);
-          break;
-        case 'Sulfuras, Hand of Ragnaros':
-          this.items[i] = this.updateLegendaryItemQuality(this.items[i]);
-          break;
-        case 'Conjured Items':
-          this.items[i] = this.updateConjuredItemQuality(this.items[i]);
-          break;
-        default:
-          this.items[i] = this.updateCustomItemQuality(this.items[i]);
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].quality > 0) {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1
+          }
+        }
+      } else {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1
+          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].sellIn < 11) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+            if (this.items[i].sellIn < 6) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+          }
+        }
+      }
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+      }
+      if (this.items[i].sellIn < 0) {
+        if (this.items[i].name != 'Aged Brie') {
+          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].quality > 0) {
+              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality = this.items[i].quality - 1
+              }
+            }
+          } else {
+            this.items[i].quality = this.items[i].quality - this.items[i].quality
+          }
+        } else {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1
+          }
+        }
       }
     }
+
     return this.items;
   }
 }
